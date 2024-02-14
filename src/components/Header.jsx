73f5ccbbf0335d260e37040e3a0a788b17e27a2c
Ansari Irfan <115148633+Ansari-Irfan-360@users.noreply.github.com>
2024-02-14 14:49:06 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,6 @@ function Header() {
   return (
     <header>
       <h1><EditNoteOutlinedIcon style={{transform: "scale(2.1)",position:"relative" ,top:"3px"}}/> &nbsp; Quick Notes</h1>
-      <h5>by Ansari Mohammad Irfan</h5>
     </header>
   );
 }
